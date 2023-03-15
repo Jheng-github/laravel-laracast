4717d3f23844d2623d5d32cb77740f06d1de78bf
Jheng <a0960073588@gmail.com>
2023-03-15 12:04:24 +0800

    feat:完成工廠模式可以自動創建&初步的api.php ROUTE設定

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,30 @@
 <?php
 
-namespace Database\Seeders;
+//namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+//use Illuminate\Database\Seeder;
+
+// class DatabaseSeeder extends Seeder
+// {
+//     /**
+//      * Seed the application's database.
+//      */
+//     public function run(): void
+//     {
+//         // \App\Models\User::factory(10)->create();
+
+//         \App\Models\User::factory()->create([
+//             'name' => 'Test User',
+//             'email' => 'test@example.com',
+//         ]);
+//     }
+// }
+//-------------
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +33,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->count(5)->create();
     }
 }
